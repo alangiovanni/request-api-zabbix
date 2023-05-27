46be4de640f0d00802a978b48deac9ce828553f9
Alan Giovanni <alan1884@hotmail.com>
2023-05-27 11:56:55 -0300

    initial

--- projeto-emanuel/modules/zabbix.py
+++ projeto-emanuel/modules/zabbix.py
@@ -3,9 +3,9 @@
 # Data: 
 # Description: Métodos para a API do Zabbix
 
-from pyzabbix import ZabbixAPI # Módulo de chamada do Zabbix
+from pyzabbix import ZabbixAPI, ZabbixAPIException # Módulo de chamada do Zabbix
 
-def autentica(login:str, password:str, url="") -> str:
+def autentica(login:str, password:str, url:str) -> str:
     """Autenticação do Zabbix"""
     zapi = ZabbixAPI(url)
     zapi.login(login, password)
@@ -21,3 +21,18 @@ def busca_name_hosts(zapi):
     """Antiga função: BuscaName"""
     dic_hosts = zapi.host.get(output=["host", "hostids"])
     return dic_hosts
+
+def create_item(zapi, host_id, interface_id, item_name, key_name, item_type=0, value_type=0, delay_item="1m"):
+    try:
+        zapi.item.create(
+            hostid=host_id,
+            interfaceid=interface_id,
+            name=item_name,
+            key_=key_name,
+            type=item_type,
+            value_type=value_type,
+            delay=delay_item
+        )
+    except ZabbixAPIException as e:
+        return 1, e
+    return 0, "ok"
